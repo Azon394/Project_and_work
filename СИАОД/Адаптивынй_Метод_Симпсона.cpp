2c41ee6e4b7d72faf68b8de552dbc1ed2b30209f
Azon <asan.khalilov.2005@mail.ru>
2024-03-13 16:39:22 +0300

    start

---
+++ СИАОД/Адаптивынй_Метод_Симпсона.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+
+double f(double x) {
+    return (log(sin(x)) - pow(x, -2));     
+    //(log(sin(x)) - pow(x, -2)) - гладкая
+    //(1/(abs(x) + 0.01)) - острая
+    //sin(x) - переодическая
+}
+
+// функция для нахождения середины между a и b, значении функции в этой середине, нахождения грубого интегрирования по методу симпсона через эти 3 точки
+std::tuple<double, double, double> quad_simpsons_mem(double a, double fa, double b, double fb) {
+    double m = (a + b) / 2; // середина
+    double fm = f(m); // значение функции в середине
+    double result = std::abs(b - a) / 6 * (fa + 4 * fm + fb); // грубое значение интеграла 
+    return std::make_tuple(m, fm, result);
+}
+
+// рекурсивная функция для вычисления интеграла по частям
+double quad_asr(double a, double fa, double b, double fb, double eps, double whole, double m, double fm) {
+    double lm, flm, left;
+    std::tie(lm, flm, left) = quad_simpsons_mem(a, fa, m, fm); // вычисление интеграла слева
+    double rm, frm, right;
+    std::tie(rm, frm, right) = quad_simpsons_mem(m, fm, b, fb); // вычисление интеграла справа
+    double delta = left + right - whole; // считаем разницу между значением интеграла до разделения и после
+    // если разница меньше чем допустимая погрешность то возвращаем результат
+    if (std::abs(delta) <= 15 * eps) {
+        return left + right + delta / 15;
+    }
+    // если нужная разница не достигнута то делим ось X пополам и считаем отдельно интеграл левой и правой части, а потом их значения складываем
+    return quad_asr(a, fa, m, fm, eps / 2, left, lm, flm) +
+           quad_asr(m, fm, b, fb, eps / 2, right, rm, frm);
+}
+
+int main() {
+    double a, b, eps, result;
+    std::cout << "a, b, eps: " << std::endl;
+    std::cin >> a >> b >> eps; // считываем значения
+    double fa = f(a);
+    double fb = f(b);
+    double m, fm, whole;
+    std::tie(m, fm, whole) = quad_simpsons_mem(a, fa, b, fb);
+    result = quad_asr(a, fa, b, fb, eps, whole, m, fm);;
+    std::cout << "Result: " << result << std::endl;
+}
---
+++ СИАОД/Метод_Симпсона_с_пог_Рунге.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+typedef double(*pointFunc)(double);
+
+double f(double x) {
+    return (log(sin(x)) - pow(x, -2));     
+    //(log(sin(x)) - pow(x, -2)) - гладкая
+    //(1/(abs(x) + 0.01)) - острая
+    //sin(x) - переодическая
+}
+
+double simpson_integral(pointFunc f, double a, double b, int n) {
+  double h = (b-a)/n; // шаг
+  double k1 = 0, k2 = 0; // переменные для сохранения значений функции в чётных и нечётных точках
+  for (int i = 1; i < n; i += 2) { // высчитываем значения функции при чётных и нечётных точках исключая крайние точки
+    k1 += f(a + i*h);
+    k2 += f(a + (i+1)*h);
+  }
+  return h/3*(f(a) + 4*k1 + 2*k2); // считаем интеграл по формуле
+}
+
+int main() {
+    double a, b, eps, runge;
+    double s1, s;
+    int n = 2; //начальное число шагов
+    cout << "a, b, eps: " << endl;
+    cin >> a >> b >> eps; // считываем значения
+    s1 = simpson_integral(f, a, b, n); // первое приближение для интеграла
+    do {
+        s = s1; // второе приближение
+        n = 2 * n; // увеличение числа шагов в два раза,
+        s1 = simpson_integral(f, a, b, n); // считаем интеграл для нового кол-ва шагов
+        runge = (s1 - s)/15;
+    }
+    while (abs(runge) > eps);  // сравниваем приближённые значения с заданной точностью
+    cout << "\nИнтеграл = " << s1 << endl;
+}
